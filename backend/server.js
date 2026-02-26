9fc9ecf1afa6b517431f01c6c3f61a1e12a89fa5
Tanya Yadav <tanyayadav19221@gmail.com>
2026-02-26 14:56:37 +0530

    Fixed CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 // Routes
 app.get('/', (req, res) => {
